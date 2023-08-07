8827c3be720c9c810d3c4fc80276275f526281f5
Kubrianity <kubraaozerenn@gmail.com>
2023-08-07 19:34:46 +0300

    Validate form fields

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function signup() {
-        return 'Hello User';
+    public function signup(Request $request) {
+        $fieldInputs = $request->validate([
+            'name'=> ['required', 'min:3'],
+            'email'=>['required', 'email'],
+            'password'=>['required', 'min:8']
+        ]);
+        return 'Hello '. $request->name;
     }
 }
