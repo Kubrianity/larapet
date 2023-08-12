f531de95445556786a2f9f97cd73f044d315fcc4
Kubrianity <kubraaozerenn@gmail.com>
2023-08-13 02:49:52 +0300

    Create form requests for validation

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -4,17 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Pet;
 use Illuminate\Http\Request;
+use App\Http\Requests\PetRegisterFormRequest;
 
 class PetController extends Controller
 {
-    public function registerPet(Request $request) {
-        $inputFields = $request-> validate([
-            'name' => 'required',
-            'breed' => 'required',
-            'age' => 'required'
-        ]);
+    public function registerPet(PetRegisterFormRequest $request) {
+        $inputFields = $request->validated();
         $inputFields['user_id'] = auth()->id();
         Pet::create($inputFields);
-        return redirect('/');
+        return back()->withInput();
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,30 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use App\Http\Requests\UserRegisterFormRequest;
+use App\Http\Requests\UserLoginFormRequest;
 
 class UserController extends Controller
 {
-    public function signup(Request $request) {
-        $inputFields = $request-> validate([
-            'name' => ['required', 'min:3'],
-            'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required', 'min:8']
-        ]);
+    public function signup(UserRegisterFormRequest $request) {
+        $inputFields = $request-> validated();
         $inputFields['password'] = bcrypt($inputFields['password']);
         $user = User::create($inputFields);
         auth()-> login($user);
         return redirect('/profile');
     }
-    public function login(Request $request) {
-        $inputFields = $request-> validate([ 
-            'login_name' => 'required',
-            'login_password' => 'required'
-        ]);
+    public function login(UserLoginFormRequest $request) {
+        $inputFields = $request-> validated();
         if (auth()-> attempt([
             'name' => $inputFields['login_name'], 
             'password' => $inputFields['login_password']
         ])
-            ) {
+        ){
             $request-> session()-> regenerate();
             return redirect('/profile');
         }
